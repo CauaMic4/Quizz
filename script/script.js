0d5a11a44bf5f52389dc3f194105802ec90ae696
cauamicael <cauamicael@hotmail.com>
2024-03-01 01:07:55 -0300

    FInalização CSs e inicio da logica para as perguntas

--- script/script.js
+++ script/script.js
@@ -1,1 +1,103 @@
-console.log("Teste");
+// declação de variaveis
+const question = document.querySelector("#question");
+const answersBox = document.querySelector("#answers-box");
+const quizzcontainer = document.querySelector("#quizz-container");
+const scoreContainer = document.querySelector("#scoreContainer");
+const letters = ["a", "b", "c", "d"];
+let points = 0;
+let actualQuestion = 0;
+
+// perguntas
+const questions = [
+    {
+        "question": "PHP foi desenvolvido para qual fim?",
+        "answers": [
+            {
+                "answer": "back-end",
+                "correct": true
+            },
+            {
+                "answer": "front-end",
+                "correct": false
+            },
+            {
+                "answer": "Sistema operacional",
+                "correct": false
+            },
+            {
+                "answer": "Banco de dados",
+                "correct": false
+            },
+        ]
+    },
+    {
+        "question": "Uma forma de declarar variável em JavaScript:",
+        "answers": [
+            {
+                "answer": "$var",
+                "correct": false
+            },
+            {
+                "answer": "var",
+                "correct": true
+            },
+            {
+                "answer": "@var",
+                "correct": false
+            },
+            {
+                "answer": "#let",
+                "correct": false
+            },
+        ]
+    },
+    {
+        "question": "Qual o seletor de id no CSS?",
+        "answers": [
+            {
+                "answer": "#",
+                "correct": true
+            },
+            {
+                "answer": ".",
+                "correct": false
+            },
+            {
+                "answer": "@",
+                "correct": false
+            },
+            {
+                "answer": "/",
+                "correct": false
+            },
+        ]
+    },
+];
+
+// substituição do quizz para a primeira pergunta
+function init() {
+    // criar a primeira pergunta
+    createQuestion(0);
+}
+
+// criar uma pergunta
+function createQuestion(i) {
+    // limpar a questao anterior
+    const oldButtons = answersBox.querySelectorAll("button");
+
+    oldButtons.forEach(function (btn) {
+        btn.remove();
+    });
+
+    // alterar o texto da pergunta
+    const questionText = question.querySelector("#question-text");
+    const questionNumber = question.querySelector("#question-number");
+
+    questionText.textContent = questions[i].question;
+    questionNumber.textContent = i + 1;
+    
+}
+
+
+// inicialização do quizz
+init();
